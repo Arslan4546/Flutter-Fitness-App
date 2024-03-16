bb9f8c50d743de11960d1beab409153fcd65d44f
Arslan Tariq <arslantariq4546@gmail.com>
2024-03-16 12:36:05 +0500

    update project

--- lib/onboardingScreens/screen1.dart
+++ lib/onboardingScreens/screen1.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-
 import '../components/button.dart';
 import '../components/fitness_text.dart';
+
 class Screen1 extends StatefulWidget {
   const Screen1({super.key});
 
@@ -21,37 +21,36 @@ class _Screen1State extends State<Screen1> {
             child: Container(
               margin: const EdgeInsets.only(bottom: 90),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.end
-                ,
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  const MainText(xTextColor: Color(0xFFCC8FED),),
-                  Text("Everybody Can Train",style: TextStyle(color: const Color(0xFF7B6F72).withOpacity(0.9),
-                    wordSpacing: 2,
-                    fontSize: 22,
-                    fontWeight: FontWeight.w500
-
-                  ),)
-
+                  const MainText(
+                    xTextColor: Color(0xFFCC8FED),
+                  ),
+                  Text(
+                    "Everybody Can Train",
+                    style: TextStyle(
+                        color: const Color(0xFF7B6F72).withOpacity(0.9),
+                        wordSpacing: 2,
+                        fontSize: 22,
+                        fontWeight: FontWeight.w500),
+                  )
                 ],
               ),
             ),
           ),
           Flexible(
-
             child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-
-                  GestureDetector(
-                    onTap:(){
+                GestureDetector(
+                    onTap: () {
                       Navigator.pushNamed(context, "/screen2");
-
-          },
-                    child: const Button(buttonText: 'Get Started', buttonColor:  Color(0xFF92A3FD), buttonTextColor: Colors.white,)
-                  ),
-
-
-
+                    },
+                    child: const Button(
+                      buttonText: 'Get Started',
+                      buttonColor: Color(0xFF92A3FD),
+                      buttonTextColor: Colors.white,
+                    )),
               ],
             ),
           ),
